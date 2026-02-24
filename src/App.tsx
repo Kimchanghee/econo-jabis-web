b60d29e8ae1a65a4fea7edc19050f361e4f9890c
Kimchanghee <k931103@gmail.com>
2026-02-24 10:09:29 +0900

    feat: add admin route to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LanguageContext, useLanguageState } from "./hooks/useLanguage";
 import Index from "./pages/Index";
 import ArticleDetail from "./pages/ArticleDetail";
+import AdminPanel from "./pages/AdminPanel";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -29,6 +30,7 @@ const App = () => (
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/article/:id" element={<ArticleDetail />} />
+                        <Route path="/admin" element={<AdminPanel />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
